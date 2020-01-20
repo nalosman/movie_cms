4e9055b8117491e8dd5272ee157da4256159c39c
nour <nour@nours-MBP.fc.ca>
2020-01-20 16:39:06 -0500

    week3 inclass(2)

--- admin/scripts/read.php
+++ admin/scripts/read.php
@@ -10,4 +10,39 @@ function getAll($tbl){
   } else{
     return 'There was a problem accessing this info';
   }
-};
+}
+
+function getSingleMovie($tbl, $col, $id){
+  //1. TODO: finish the function based on getAll, this time only return
+  //one movie's field   ; all fields from one movie
+  $pdo = Database::getInstance()->getConnection();
+  $querySingle = 'SELECT * FROM '.$tbl.' WHERE '.$col.' = '.$id;
+  //$querySingle = "SELECT * FROM $tbl WHERE $col = $id";
+  $resultSingle= $pdo->query($querySingle);
+
+  if($resultSingle) {
+    return $resultSingle;
+  }else {
+    return 'There was a problem getting this information';
+  }
+}
+
+function getMoviesByFilter($args){
+  $pdo = Database::getInstance()->getConnection();
+
+  $filterQuery = 'SELECT * FROM '.$args['tbl'].' AS t, '.$args['tbl2'].' AS t2, '.$args['tbl3'] .' AS t3';
+  $filterQuery .= ' WHERE t.'.$args['col'].' = t3.'.$args['col'];
+  $filterQuery .= ' AND t2.'.$args['col2'].' = t3.'.$args['col2'];
+  $filterQuery .= ' AND t2.'.$args['col3'].' = "'.$args['filter'].'"';
+
+  // echo $filterQuery;
+  // exit;
+
+  $results = $pdo->query($filterQuery);
+
+  if($results) {
+    return $results;
+  }else{
+    return 'There was a problem accessing this info';
+  }
+}
---
+++ load.php
@@ -0,0 +1,12 @@
+<?php 
+
+define('ABSPATH', __DIR__);   //SHOW WHERE FILE IS 
+define('ADMIN_PATH', ABSPATH.'/admin');
+define('ADMIN_SCRIPT_PATH', ADMIN_PATH.'/scripts');
+
+// echo ABSPATH;
+// exit;
+
+ini_set('display_errors', 1);
+require_once ABSPATH.'/config/database.php';
+require_once ADMIN_SCRIPT_PATH.'/read.php';
