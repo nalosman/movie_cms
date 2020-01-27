75c32ea15ffd8ce8c81381367f36c6513eae6071
nour <nour@nours-MBP.fc.ca>
2020-01-27 16:02:17 -0500

    started login, .htaccess

--- load.php
+++ load.php
@@ -10,3 +10,7 @@ define('ADMIN_SCRIPT_PATH', ADMIN_PATH.'/scripts');
 ini_set('display_errors', 1);
 require_once ABSPATH.'/config/database.php';
 require_once ADMIN_SCRIPT_PATH.'/read.php';
+require_once ADMIN_SCRIPT_PATH.'/login.php';
+
+
+//loads functions to where they need to go go, A Bridge
